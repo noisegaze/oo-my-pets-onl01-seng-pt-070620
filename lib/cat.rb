4fd04630a8910ddeed8a4f393a5bb818e0e76cde
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-07 21:34:05 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,11 +3,16 @@ class Cat
   attr_accessor :owner, :mood 
   attr_reader :name
   
+  @@all = []
   
   def initialize(name,owner)
     @name = name 
     @owner = owner 
     @mood = "nervous"
+  end
+  
+  def save 
+    @@all << self 
   end
   
   
