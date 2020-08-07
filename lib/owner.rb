27199774c09793fcb9780528a51053d1f14ca9aa
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-07 22:57:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,7 +60,7 @@ class Owner
   
   def sell_pets 
     cats.each do |cat|
-      cat.mood = "nervous" cat.owner = nil 
+      cat.mood = "nervous" && cat.owner = nil 
      end 
      dogs.each do |dog| 
        dog.mood = "nervous" && dog.owner = nil
